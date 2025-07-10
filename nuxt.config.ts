cde582e24dc5cfa3dda24251c858fb71f4dd79a8
KurutoDenzeru <kurtdenzel51@gmail.com>
2025-07-09 19:46:17 -0700

    feat: add configuration files for Nuxt and Tailwind; create palette types; remove unused files

---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    'nuxt-vitalizer',
+    'shadcn-nuxt',
+    '@pinia/nuxt'
+  ],
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  // Nuxt Vitalizer
+  vitalizer: {
+    // Remove the render-blocking entry CSS
+    disableStylesheets: 'entry',
+    // Remove preloading build resources
+    disablePrefetchLinks: true
+  },
+
+  // Shadnc Nuxt
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
---
+++ types/palette.ts
@@ -0,0 +1,14 @@
+export type PaletteMode =
+  | 'analogous'
+  | 'monochrome'
+  | 'complementary'
+  | 'triadic'
+  | 'compound'
+  | 'shades'
+  | 'tetradic'
+  | 'square'
+
+export type PaletteColor = {
+  hex: string
+  rgba: string
+}
